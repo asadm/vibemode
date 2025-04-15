0ac61137d572e9371b1fbb2eacdf5c2bf00c5233
Asad Memon <asadmemon@google.com>
2025-04-15 15:11:09 -0700

    some testing files

---
+++ testfixtures/a.js
@@ -0,0 +1,12 @@
+export function add(a, b) {
+    console.log(add(1, 2))
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
---
+++ testfixtures/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from "./a";
+
+
+function run(){
+    console.log(add(1, 2));
+    console.log(subtract(1, 2));
+}
